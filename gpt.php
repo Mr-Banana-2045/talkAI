af32a73e535e27b1228308d0b787c81a9aaa4b97
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-12-26 23:46:28 +0330

    Update gpt.php

--- gpt.php
+++ gpt.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 
 error_reporting(0);
 ini_set('display_errors', 0);
@@ -7,32 +7,57 @@ ini_set('display_errors', 0);
 $url = "https://req.wiki-api.ir/apis-1/ChatGPT?q=" . urlencode($_GET['text']);
 
 $ch = curl_init($url);
-
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Accept-Charset: utf-8'
 ]);
 
 $response = curl_exec($ch);
-
 if ($response === false) {
-    die('An error occurred.');
+    echo json_encode(['error' => 'An error occurred.']);
+    exit;
 }
 
 curl_close($ch);
-
 $data = json_decode($response, true);
-
 if (json_last_error() !== JSON_ERROR_NONE) {
-    die('An error occurred.');
+    echo json_encode(['error' => 'An error occurred while decoding JSON.']);
+    exit;
 }
 
 if (isset($data['results'])) {
-   $results = $data['results'];
-   $dec = ['kh' => 'خ', 'oo' => 'و', 'b' => 'ب', 'y' => 'ی', 'i' => 'ی', 'sa' => 'س', 'a' => 'ا', 'l' => 'ل', 'm' => 'م', 'd' => 'د', 't' => 'ت','gh' => 'ق', 'ghe' => 'غ', 'he' => 'ح', 'tt' => 'ط', 'j' => 'ج', 'n' => 'ن', 'see' => 'ص', 'r' => 'ر', 'ze' => 'ظ', 'zee' => 'ذ', 'zz' => 'ض', 'ss' => 'ث', 'zhe' => 'ژ', 'che' => 'چ', 'c' => 'ک', 'h' => 'ه', 'z' => 'ز', 'f' => 'ف', 'p' => 'پ', 'k' => 'ک', 'sh' => 'ش', 'g' => 'گ', 'e' => '', 'ea' => 'ع', '.' => '؟', '   ' => '،', ' ' => '!', '  ' => '```', '' => '😊'];
-   $res = strtr($results, array_flip($dec));
-   echo $res;
+    $results = $data['results'];
+    $del = ['' => '😊'];
+    $translatedText = strtr($results, array_flip($del));
+
+    $translateUrl = "https://api.codebazan.ir/translate/?type=json&from=fa&to=en&text=" . urlencode($results);
+    
+    $chTranslate = curl_init($translateUrl);
+    curl_setopt($chTranslate, CURLOPT_RETURNTRANSFER, true);
+    
+    $translateResponse = curl_exec($chTranslate);
+    if ($translateResponse === false) {
+        echo json_encode(['error' => 'An error occurred while calling Codebazan API.']);
+        exit;
+    }
+
+    curl_close($chTranslate);
+    $translateData = json_decode($translateResponse, true);
+    
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        echo json_encode(['error' => 'An error occurred while decoding translation JSON.']);
+        exit;
+    }
+
+    if (isset($translateData['translation'])) {
+        echo json_encode([
+            'fa' => strtr($results, array_flip($del)),
+            'en' => strtr($translateData['translation'], array_flip($del))
+        ]);
+    } else {
+        echo json_encode(['error' => 'No translation found.']);
+    }
 } else {
-    echo "No results found.";
+    echo json_encode(['error' => 'No results found.']);
 }
 ?>
